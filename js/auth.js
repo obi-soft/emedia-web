c50a22a8a6522948530ae2ecdac863ef4a7c8cc9
obi-soft <ltd.obisoft@gmail.com>
2025-12-17 21:08:49 +0100

    Implement login and registration functions

---
+++ js/auth.js
@@ -0,0 +1,27 @@
+import { auth } from './firebase.js';
+import {
+signInWithEmailAndPassword,
+createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+window.login = function () {
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+
+signInWithEmailAndPassword(auth, email, password)
+.then(() => window.location = 'home.html')
+.catch(err => alert(err.message));
+};
+
+
+window.register = function () {
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+
+createUserWithEmailAndPassword(auth, email, password)
+.then(() => window.location = 'home.html')
+.catch(err => alert(err.message));
+};
